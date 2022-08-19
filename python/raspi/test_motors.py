f5fe0fbb559c8bd59f22bd213f486ef540dabfd5
hakfossa <hakfossa@gmail.com>
2022-08-19 11:03:15 +0200

    create serial object

--- python/raspi/test_motors.py
+++ python/raspi/test_motors.py
@@ -1,5 +1,5 @@
 
-import imrt_robot_serial as rs
+import imrt_robot_serial
 import signal
 import time
 import sys
@@ -18,7 +18,7 @@ def stop_robot(duration):
     iterations = int(duration * 10)
     
     for i in range(iterations):
-        rs.motor_serial.send_command(0, 0)
+        motor_serial.send_command(0, 0)
         time.sleep(0.10)
 
 def drive_robot(direction, duration):
@@ -27,12 +27,14 @@ def drive_robot(direction, duration):
     iterations = int(duration * 10)
 
     for i in range(iterations):
-        rs.motor_serial.send_command(speed, speed)
+        motor_serial.send_command(speed, speed)
         time.sleep(0.10)
 
+# Create motor serial
+motor_serial = imrt_robot_serial.IMRTRobotSerial()
 
 try:
-    rs.motor_serial.connect("/dev/ttyACM0")
+    motor_serial.connect("/dev/ttyACM0")
 except Exception as e:
     print(e)
     print("Could not open port. Is your robot connected?\nExiting program")
@@ -40,10 +42,10 @@ except Exception as e:
 
     
 # Start serial receive thread
-rs.motor_serial.run()
+motor_serial.run()
 
 print("Entering loop. Ctrl+c to terminate")
-while not rs.motor_serial.shutdown_now:
+while not motor_serial.shutdown_now:
     
     drive_robot(FORWARDS, 1)
 
