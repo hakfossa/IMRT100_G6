9a720c2323a1a430557bfc2721965245bc270e38
Katla Maria Meyer <katla.maria.meyer@nmbu.no>
2022-08-24 10:51:00 +0200

    tweaks

--- python/raspi/km_test.py
+++ python/raspi/km_test.py
@@ -1,4 +1,5 @@
 # Py Modules
+from re import A
 import signal
 import time
 import sys
@@ -19,6 +20,19 @@ STOP_DISTANCE = 25
 
 tfreq = 10 # Timer Frequency, Execution frequency in Hz
 tstep = 1/tfreq # Timer Step length
+
+DX_fwd = []
+DX_bck = []
+DX_r = []
+DX_l = []
+DXlength = 10
+
+# Go through the sensors and give them an appropriate amount of nothing
+valid_sensors = [DX_fwd,DX_bck,DX_r,DX_l]
+for sensor in valid_sensors:
+    for i in range(DXLength):
+        sensor.append(255)
+print("Sensor arrays filled with gibberish")
 
 # Motile functions
 
@@ -52,18 +66,36 @@ except:
 # Start serial receive thread
 motor_serial.run()
 
-# Exec loop?
-while not motor_serial.shutdown_now:
+# Defining a function that retrieves sensor data and updates the time avg. array for each sensor
+def sense():
+    # Sensor definitions
     dist_fwd = motor_serial.get_dist_1()
-    dist_back = motor_serial.get_dist_2
+    dist_back = motor_serial.get_dist_2()
     dist_r = motor_serial.get_dist_3()
     dist_l = motor_serial.get_dist_4()
     print("fwd:", dist_fwd, "bck:", dist_back, "R:", dist_r, "L:", dist_l)
+    
+    DX_fwd.append(dist_fwd) # Add most recent value
+    DX_fwd.pop(0) # Delete oldest value
 
-    # Check if there is an obstacle in the way
-    if dist_fwd < STOP_DISTANCE:
-        # There is an obstacle in front of the robot
-        # First let's stop the robot for 1 second
+    DX_bck.append(dist_back)
+    DX_bck.pop(0)
+
+    DX_r.append(dist_r)
+    DX_r.pop(0)
+
+    DX_l.append(dist_l)
+    DX_r.pop(0)
+
+    time.sleep(tstep)
+
+# Exec loop
+while not motor_serial.shutdown_now:
+
+    sense()
+
+    # Obstacle check
+    if DX_fwd < STOP_DISTANCE:
         print("halt")
         stop_robot(tstep)
     else:
