c8daa218c1641cfc3aadbf259910712a11c1eb98
Katla Maria Meyer <katla.maria.meyer@nmbu.no>
2022-08-24 10:48:28 +0200

    added primitive sensor parser thingy

--- python/raspi/km_test.py
+++ python/raspi/km_test.py
@@ -20,8 +20,6 @@ STOP_DISTANCE = 25
 tfreq = 10 # Timer Frequency, Execution frequency in Hz
 tstep = 1/tfreq # Timer Step length
 
-
-
 # Motile functions
 
 def stop_robot(duration):
@@ -41,30 +39,34 @@ def turn_robot(direction, duration):
     motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
     time.sleep(tstep)
 
-imrt_connect.handshake()
+# Create motor serial object
+motor_serial = imrt_robot_serial.IMRTRobotSerial()
+
+# Open serial port. Exit if serial port cannot be opened
+try:
+    motor_serial.connect("/dev/ttyACM0")
+except:
+    print("Could not open port. Is your robot connected?\nExiting program")
+    sys.exit()
+
+# Start serial receive thread
+motor_serial.run()
 
 # Exec loop?
 while not motor_serial.shutdown_now:
-    dist_1 = motor_serial.get_dist_1()
-    dist_2 = motor_serial.get_dist_2()
-    print("Dist 1:", dist_1, "   Dist 2:", dist_2)
+    dist_fwd = motor_serial.get_dist_1()
+    dist_back = motor_serial.get_dist_2
+    dist_r = motor_serial.get_dist_3()
+    dist_l = motor_serial.get_dist_4()
+    print("fwd:", dist_fwd, "bck:", dist_back, "R:", dist_r, "L:", dist_l)
 
     # Check if there is an obstacle in the way
-    if dist_1 < STOP_DISTANCE or dist_2 < STOP_DISTANCE:
+    if dist_fwd < STOP_DISTANCE:
         # There is an obstacle in front of the robot
         # First let's stop the robot for 1 second
-        print("Obstacle!")
-        stop_robot(1)
-
-        # Reverse for 0.5 second
-        drive_robot(BACKWARDS, 0.5)
-
-        # Turn random angle
-        turn_robot_random_angle()
-        
-
+        print("halt")
+        stop_robot(tstep)
     else:
-        # If there is nothing in front of the robot it continus driving forwards
-        drive_robot(FORWARDS, 0.1)
+        drive_robot(FORWARDS, tstep)
 
 print("Bye!")
