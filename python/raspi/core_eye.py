d8899df15a517efa1d668db6080da6cf6e901277
hakfossa <hakfossa@gmail.com>
2022-08-24 12:06:39 +0200

    black

--- python/raspi/core_eye.py
+++ python/raspi/core_eye.py
@@ -21,7 +21,7 @@ BLACK = '#000000'
 screenX = 800
 screenY = 480
 DISPLAY = pygame.display.set_mode((screenX, screenY))
-DISPLAY.fill(BLACK)
+DISPLAY.fill("#000000")
 pygame.display.set_caption("space_core")
 
 class Core_eye(pygame.sprite.Sprite):
