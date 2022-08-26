8ead0aa24e571580b65663771318e44f70b85afb
hakfossa <hakfossa@gmail.com>
2022-08-26 14:30:04 +0200

    Crack

--- python/raspi/km_test.py
+++ python/raspi/km_test.py
@@ -366,7 +366,7 @@ for buffer in chgbuffer_list:
 initialfill = False
 print("Buffers filled with gibberish.")
 
-startup_timer = 30
+startup_timer = 50
 rotated_dist = [0, 0, 0, 0]
 
 # Main loop
@@ -382,7 +382,7 @@ while not motor_serial.shutdown_now:
 
 #    print(" FWD:",round(sense_fwd(),1),"BCK:",round(sense_bck(),1),"R:",(round(sense_r(),1),"L:",round(sense_l(),1)))
 #    print("aFWD:",round(avg_fwd(),1),"aBCK:",round(avg_bck(),1),"aR:",round(avg_r(),1),"aL:",round(avg_l(),1))
-    print("chg_r =", chg_r(), "chg_l = ", chg_l())
+#    print("chg_r =", chg_r(), "chg_l = ", chg_l())
     # Obstacle check
     if sense_fwd() < STOP_DISTANCE:
         print("Holding")
