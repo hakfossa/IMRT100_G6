568eaebb02523ba7dc3efe1151d37ab0d1f72eee
hakfossa <hakfossa@gmail.com>
2022-08-27 14:28:16 +0200

    follow_right

--- python/raspi/follow_right.py
+++ python/raspi/follow_right.py
@@ -71,15 +71,22 @@ while not motor_serial.shutdown_now :
 
     sensor_fwd = motor_serial.get_dist_1()
     sensor_right = motor_serial.get_dist_3()
-
+    sensor_left = motor_serial.get_dist_4()
 
 
     if sensor_fwd < 15:
         #stop_robot(1)
-        turn_robot(RIGHT,1.8)
+        if sensor_left >sensor_right*2:
+            turn_robot(LEFT,1.8)
+        else:
+            turn_robot(RIGHT, 1.8)
 
     elif sensor_right < 15:
         turn_robot(LEFT,0.5)
+        drive_robot(FORWARDS, 0.5)
+
+    elif sensor_left < 15:
+        turn_robot(RIGHT, 0.5)
         drive_robot(FORWARDS, 0.5)
 
     else: 
