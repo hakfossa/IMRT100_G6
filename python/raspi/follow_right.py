4cfde0e547ee0b02c6af3aee3e4c28f3df8ccd77
hakfossa <hakfossa@gmail.com>
2022-08-27 16:13:27 +0200

    follow_right

--- python/raspi/follow_right.py
+++ python/raspi/follow_right.py
@@ -79,7 +79,7 @@ while not motor_serial.shutdown_now :
 
     # Kjøre fremover
     if sensor_fwd < 25:
-        turn_timer=-10
+        turn_timer = -1
         #stop_robot(1)
         if sensor_left >sensor_right*2:
             turn_robot(LEFT,1.8)
