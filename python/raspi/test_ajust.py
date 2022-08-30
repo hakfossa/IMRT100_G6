7f74c0f68813115357e33070b02ca82da05a05b4
hakfossa <hakfossa@gmail.com>
2022-08-30 12:48:18 +0200

    90degtest

--- python/raspi/test_ajust.py
+++ python/raspi/test_ajust.py
@@ -71,7 +71,7 @@ while not motor_serial.shutdown_now :
     sensor_rfwd = motor_serial.get_dist_1() 
     sensor_lfwd = motor_serial.get_dist_2()
 
-    print(sensor_lfwd, print(sensor_rfwd))
+    print(sensor_lfwd, sensor_rfwd)
 
     ajust_90deg(sensor_lfwd, sensor_rfwd)
 
