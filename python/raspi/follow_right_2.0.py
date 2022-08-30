c99b601afce681d49a643ca299d4ae10976929d8
hakfossa <hakfossa@gmail.com>
2022-08-30 13:41:04 +0200

    smoothturns

--- python/raspi/follow_right_2.0.py
+++ python/raspi/follow_right_2.0.py
@@ -181,7 +181,7 @@ while not motor_serial.shutdown_now :
 
     # svinge inn i åpning høyre
     elif turn_timer == 0:
-        smooth_turn(RIGHT, 1.7)
+        smooth_turn(RIGHT, 1.4)
         turn_timer -= 1
 
     # se åpning høyre
